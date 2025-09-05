449d3c5e209e81f54ce5e17368fed0e006aea020
ujwal-devpX <sujwals2005@gmail.com>
2025-09-05 22:26:45 +0530

    User data model updated

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -7,25 +7,41 @@ const userSchema = new mongoose.Schema({
     unique: true,
     trim: true,
   },
+  fullname: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true,
+  },
   email: {
     type: string,
     required: true,
     unique: true,
+    trim: true,
   },
   password: {
     type: String,
+    required: [true, "Password is required"],
+    minlength: 6,
+  },
+  avatar: {
+    type: String, // storedCloudinary
     required: true,
-    minlength: 6, // password should be at least 6 characters
   },
-  role: {
+  coverImage: {
     type: String,
-    enum: ["user", "admin"], // only allowed values
-    default: "user",
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
+  watchHistory: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+  ],
+  refreshToken: {
+    type: String,
   },
+},{
+  timestamps:true
 });
 
 export const user = mongoose.model("user", userSchema);
