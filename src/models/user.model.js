767d35c6a0e65ebebb80274297be61571c87a300
ujwal-devpX <sujwals2005@gmail.com>
2025-09-07 16:27:16 +0530

    Hashing of password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,47 +1,61 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  fullname: {
-    type: String,
-    required: true,
-    trim: true,
-    index: true,
-  },
-  email: {
-    type: string,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    minlength: 6,
-  },
-  avatar: {
-    type: String, // storedCloudinary
-    required: true,
-  },
-  coverImage: {
-    type: String,
-  },
-  watchHistory: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Video",
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-  ],
-  refreshToken: {
-    type: String,
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: string,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+    avatar: {
+      type: String, // storedCloudinary
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    refreshToken: {
+      type: String,
+    },
   },
-},{
-  timestamps:true
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
 });
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+   return await bcrypt.compare(password, this.password);
+};
 
 export const user = mongoose.model("user", userSchema);
