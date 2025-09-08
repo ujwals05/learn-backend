77a141ca1b220ce852eaf3a6be41cbc3f3382f0c
ujwal-devpX <sujwals2005@gmail.com>
2025-09-08 18:56:33 +0530

    small changes

--- src/utils/apierror.js
+++ src/utils/apierror.js
@@ -1,13 +1,13 @@
 class APIError extends Error {
-  constructor(statusCode, message, errors = [], statck = "") {
+  constructor(statusCode, message, errors = [], stack = "") {
     super(message);
     this.message = message;
     this.statusCode = statusCode;
     this.errors = errors;
     this.data = false;
     this.success = false;
-    if(statck)
-      this.statck = stack
+    if(stack)
+      this.stack = stack;
     else
       Error.captureStackTrace(this,this.constructor)
   }
