ca9551fc6535860756dde093fa23f52f72b49524
ujwal-devpX <sujwals2005@gmail.com>
2025-09-08 23:31:41 +0530

    file uploading from server to cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const cloudUpLoad = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    //uploading the file
+    const result = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    //File uploaded successfully
+    console.log("The file has been uploaded successfully", result.url);
+    return result;
+  } catch (error) {
+    console.log("There is an error in uploading the file");
+    fs.unlinkSync(localFilePath); //remove the locally saved temp file as the upload operation is failed
+    return null;
+  }
+};
+
+export default cloudUpLoad;
