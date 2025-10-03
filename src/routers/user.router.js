5a7f4404300d63695978ea54cd5a9ce6493d5482
ujwal-devpX <sujwals2005@gmail.com>
2025-10-04 01:24:06 +0530

    routers written for all the controllers

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -4,6 +4,13 @@ import {
   registerUser,
   loginUser,
   refreshAccessToken,
+  changePassword,
+  currentUser,
+  updateUser,
+  updateAvatar,
+  updateCoverImage,
+  userChannelProfile,
+  userWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
@@ -29,8 +36,20 @@ router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT, logoutUser);
 
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
 
+router.route("/change-password").patch(verifyJWT, changePassword);
+
+router.route("/current-user").get(verifyJWT, currentUser);
+
+router.route("/update-user").patch(verifyJWT, updateUser);
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+router.route("/c/:username").get(verifyJWT, userChannelProfile);
+
+router.route("/watch-history").get(verifyJWT, userWatchHistory);
 
 export default router;
-
